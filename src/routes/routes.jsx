433cb8ca47df2603d0d1d4409c99e3d98e9bc55a
apridew <dewiapriyantiputra@gmail.com>
2024-02-01 07:50:22 +0700

    fix route page

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,7 +2,7 @@ import DetailCarPage from "../pages/DetailCarPage";
 import LandingPage from "../pages/LandingPage";
 import NotFound from "../pages/NotFound";
 import PaymentPage from "../pages/PaymentPage";
-import PaymentUploadPage from "../pages/PaymentUploadPage copy";
+import PaymentUploadPage from "../pages/PaymentUploadPage";
 import RegisterPage from "../pages/RegisterPage";
 import SearchCarPage from "../pages/SearchCarPage";
 import SignInPage from "../pages/SignInPage";
